3217cd31331c60bcb2ad327f5e9be5c32764d625
jpkha <jeanphilippe.kha@gmail.com>
2022-07-08 16:53:09 +0200

    feat: create storyblok heroicons fieldtype

--- README.md
+++ README.md
@@ -1,35 +1,72 @@
-# Storyblok Field-Type
+# Storyblok Field-Type Heroicons
 
-> A Storyblok Field-Type Plugin Starter Project
 
-## Project setup
+This field-type allows users to choose icons with each of the available icon styles from [Heroicons](https://heroicons.com/).
+
+Name | Description                            | Author
+------------ |----------------------------------------| -------------
+Heroicons | Allows users to select any heroic icon | [Jean-Philippe Kha](https://github.com/jpkha)
+
+![img.png](img.png)
+## Usage
+
+Select the desired style, and search/filter for your desired icon. You have access to the solid and outline Heroicons.
+
+Returns an object with the class and icon name:
 ```
-npm install
+{
+  "plugin": "heroicon",
+  "icon": "<icon name>",
+  "svgIcon": "<svg path>",
+  "type": "heroicon-outline | heroicon-solid",
+  ...
+}
+```
+Example : 
+```
+{
+  "plugin": "heroicon",
+  "icon": "annotation",
+  "svgIcon": "<svg xmlns=\"http://www.w3.org/2000/svg\" fill=\"none\" viewBox=\"0 0 24 24\" stroke=\"currentColor\"><path stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"2\" d=\"M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z\"/></svg>",
+  "type": "heroicon-outline",
+  ...
+}
 ```
 
-### Compiles and hot-reloads for development
+## Output - Use the icon name 
+
+You can load the script :
 ```
-npm run serve
+<script src="https://unpkg.com/hero-icon-js/hero-icon-outline.js"></script>
+<script src="https://unpkg.com/hero-icon-js/hero-icon-solid.js"></script>
+```
+Then use the icon name from Heroic Icon object :
+```
+<hero-icon-outline name="{blok.icon}"></hero-icon-outline>
+<hero-icon-solid name="{blok.icon}"></hero-icon-solid>
 ```
 
-In the browser, you will see the white screen with the following error in the console:
-```
-[Vue warn]: Error in created hook: "SyntaxError: Failed to execute 'postMessage' on 'Window': Invalid target origin 'null//app.storyblok.com' in a call to 'postMessage'."
+## Output - Use svg Icon path
+By using svgIcon you can have the source of the svg, so you don't need to load any resources to display it
 
-DOMException: Failed to execute 'postMessage' on 'Window': Invalid target origin 'null//app.storyblok.com' in a call to 'postMessage'.
+Exemple in Vue.js
 ```
-But don't worry, this project is just a template and only works as part of the storyblok app. See: 
-https://www.storyblok.com/docs/plugins/enable-local-dev-mode
-
-### Compiles and minifies for production
-```
-npm run build
+<span v-html="svgIcon"></span>
 ```
 
-### Lints and fixes files
+Exemple in Angular
 ```
-npm run lint
+<div [innerHTML]="svgIcon"></div>
 ```
 
-### Customize configuration
-https://www.storyblok.com/docs/Guides/Creating-a-field-type-plugin#how-to-develop-plugins-locally
+Exemple in React
+```
+<span dangerouslySetInnerHTML={{__html: svgIcon}}></span>
+```
+
+## Styling Svg icon
+
+You can style your icon by using class or style with Height, Width, Color property for example.
+```
+<span style="height: 24px; width: 24px; color: blue; display: inline-block" v-html="getSvg(model)"></span>
+```
--- src/Plugin.vue
+++ src/Plugin.vue
@@ -1,38 +1,90 @@
-<template>
-  <div>
-    Google snippet preview:
-    <div class="p-metatags__preview">
-      <div class="p-metatags__google-title">{{ model.title || 'Your title' }}</div>
-      <div class="p-metatags__google-link">yoursite.com/example</div>
-      <div class="p-metatags__google-description">{{ model.description || 'Your description' }}</div>
-    </div>
-    <div class="uk-form-row">
-      <label>Meta Title</label>
-      <input type="text" placeholder="Your title" v-model="model.title" class="uk-width-1-1">
-    </div>
-
-    <div class="uk-form-row">
-      <label>Meta description</label>
-      <textarea rows="4" placeholder="Your description" v-model="model.description" class="uk-width-1-1"></textarea>
-    </div>
-  </div>
-</template>
-
 <script>
 export default {
   mixins: [window.Storyblok.plugin],
+  template: `
+    <div>
+    <div class="uk-margin-bottom uk-text-center" v-if="model.icon">
+      Current icon:
+      <div>
+        <span style="margin: 5px; height: 24px; width: 24px; display: inline-block;" v-html="getSvg(model)"></span>
+        <small style="cursor:pointer;" @click="removeIcon">(remove)</small>
+      </div>
+    </div>
+    <input class="uk-width-1-1 uk-margin-bottom" v-model="search" placeholder="Search icons"/>
+    <span class="icon-link" @click="setIcon(icon)" :key="icon.icon + icon.type" v-for="icon in filteredIcons"
+          style="margin: 5px; height: 24px; width: 24px; display: inline-block;">
+      <span v-html="getSvg(icon)"></span>
+  </span>
+    </div>`,
+  data() {
+    return {
+      search: '',
+      iconsName: ["academic-cap", "adjustments", "annotation", "archive", "arrow-circle-down", "arrow-circle-left", "arrow-circle-right", "arrow-circle-up", "arrow-down", "arrow-left", "arrow-narrow-down", "arrow-narrow-left", "arrow-narrow-right", "arrow-narrow-up", "arrow-right", "arrow-sm-down", "arrow-sm-left", "arrow-sm-right", "arrow-sm-up", "arrow-up", "arrows-expand", "at-symbol", "backspace", "badge-check", "ban", "beaker", "bell", "book-open", "bookmark-alt", "bookmark", "briefcase", "cake", "calculator", "calendar", "camera", "cash", "chart-bar", "chart-pie", "chart-square-bar", "chat-alt-2", "chat-alt", "chat", "check-circle", "check", "chevron-double-down", "chevron-double-left", "chevron-double-right", "chevron-double-up", "chevron-down", "chevron-left", "chevron-right", "chevron-up", "chip", "clipboard-check", "clipboard-copy", "clipboard-list", "clipboard", "clock", "cloud-download", "cloud-upload", "cloud", "code", "cog", "collection", "color-swatch", "credit-card", "cube-transparent", "cube", "currency-bangladeshi", "currency-dollar", "currency-euro", "currency-pound", "currency-rupee", "currency-yen", "cursor-click", "database", "desktop-computer", "device-mobile", "device-tablet", "document-add", "document-download", "document-duplicate", "document-remove", "document-report", "document-search", "document-text", "document", "dots-circle-horizontal", "dots-horizontal", "dots-vertical", "download", "duplicate", "emoji-happy", "emoji-sad", "exclamation-circle", "exclamation", "external-link", "eye-off", "eye", "fast-forward", "film", "filter", "finger-print", "fire", "flag", "folder-add", "folder-download", "folder-open", "folder-remove", "folder", "gift", "globe-alt", "globe", "hand", "hashtag", "heart", "home", "identification", "inbox-in", "inbox", "information-circle", "key", "library", "light-bulb", "lightning-bolt", "link", "location-marker", "lock-closed", "lock-open", "login", "logout", "mail-open", "mail", "map", "menu-alt-1", "menu-alt-2", "menu-alt-3", "menu-alt-4", "menu", "microphone", "minus-circle", "minus-sm", "minus", "moon", "music-note", "newspaper", "office-building", "paper-airplane", "paper-clip", "pause", "pencil-alt", "pencil", "phone-incoming", "phone-missed-call", "phone-outgoing", "phone", "photograph", "play", "plus-circle", "plus-sm", "plus", "presentation-chart-bar", "presentation-chart-line", "printer", "puzzle", "qrcode", "question-mark-circle", "receipt-refund", "receipt-tax", "refresh", "reply", "rewind", "rss", "save-as", "save", "scale", "scissors", "search-circle", "search", "selector", "server", "share", "shield-check", "shield-exclamation", "shopping-bag", "shopping-cart", "sort-ascending", "sort-descending", "sparkles", "speakerphone", "star", "status-offline", "status-online", "stop", "sun", "support", "switch-horizontal", "switch-vertical", "table", "tag", "template", "terminal", "thumb-down", "thumb-up", "ticket", "translate", "trash", "trending-down", "trending-up", "truck", "upload", "user-add", "user-circle", "user-group", "user-remove", "user", "users", "variable", "video-camera", "view-boards", "view-grid-add", "view-grid", "view-list", "volume-off", "volume-up", "wifi", "x-circle", "x", "zoom-in", "zoom-out"],
+      heroIconsOutline: undefined,
+      heroIconsSolid: undefined
+    }
+  },
+  computed: {
+    filteredIcons() {
+      let iconsName = this.iconsName.filter((icon) => {
+        return icon.indexOf(this.search) > -1
+      })
+
+      let result = []
+      iconsName.forEach((icon) => {
+        result.push({icon: icon, type: 'heroicon-outline'})
+        result.push({icon: icon, type: 'heroicon-solid'})
+      })
+      return result.slice(0, 25)
+    }
+  },
   methods: {
-    initWith() {
-      return {
-        // needs to be equal to your storyblok plugin name
-        plugin: 'my-plugin-name',
-        title: '',
-        description: ''
+    getSvg(icon) {
+      if (icon.type === 'heroicon-outline' && this.heroIconsOutline) {
+        return `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">${this.heroIconsOutline.icons[icon.icon]}</svg>`
+      } else if (icon.type === 'heroicon-solid' && this.heroIconsSolid) {
+        return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">${this.heroIconsSolid.icons[icon.icon]}</svg>`
+      } else {
+        return ''
       }
     },
+    initWith() {
+      return {
+        plugin: 'heroicon',
+        icon: '',
+        svgIcon: '',
+        type: ''
+      }
+    },
+    setIcon(icon) {
+      this.model.icon = ''
+      this.$nextTick(() => {
+        this.model.icon = icon.icon
+        this.model.svgIcon = this.getSvg(icon)
+        this.model.type = icon.type
+      });
+    },
+    removeIcon() {
+      this.model.icon = ''
+      this.$nextTick(() => {
+        this.model.icon = ''
+        this.model.svgIcon = ''
+        this.model.type = ''
+      });
+    },
     pluginCreated() {
-      // eslint-disable-next-line
-      console.log('View source and customize: https://github.com/storyblok/storyblok-fieldtype')
+      this.$sb.getScript('https://cdn.jsdelivr.net/npm/hero-icon-js/hero-icon-outline.min.js', () => {
+        // eslint-disable-next-line
+        const heroIconsOutline = new HeroIconOutline();
+        // eslint-disable-next-line
+        this.heroIconsOutline = heroIconsOutline;
+      });
+      this.$sb.getScript('https://cdn.jsdelivr.net/npm/hero-icon-js/hero-icon-solid.js', () => {
+        // eslint-disable-next-line
+        const heroIconsSolid = new HeroIconSolid();
+        // eslint-disable-next-line
+        this.heroIconsSolid = heroIconsSolid;
+      })
     }
   },
   watch: {
@@ -45,21 +97,3 @@ export default {
   }
 }
 </script>
-
-<style>
-  .p-metatags__google-title {
-    color: blue;
-    text-decoration: underline;
-  }
-
-  .p-metatags__google-link {
-    color: green;
-  }
-
-  .p-metatags__preview {
-    margin: 5px 0 15px;
-    padding: 10px;
-    color: #000;
-    background: #FFF;
-  }
-</style>
